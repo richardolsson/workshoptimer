3034a0d1ae581f4880fd6565dac48f68033a862c
Richard Olsson <r@richardolsson.se>
2022-12-19 13:45:21 +0100

    Add timeScale for useTimer (useful during development)

--- src/useTimer.ts
+++ src/useTimer.ts
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
 
+type UseTimerOptions = {
+  timeScale?: number;
+};
+
 type UseTimerReturn = {
   isRunning: boolean;
   secondsElapsed: number;
@@ -8,22 +12,27 @@ type UseTimerReturn = {
   stop: () => void;
 };
 
-export default function useTimer(duration: number): UseTimerReturn {
+export default function useTimer(
+  duration: number,
+  opts?: UseTimerOptions
+): UseTimerReturn {
   const [startTime, setStartTime] = useState<Date | null>(null);
   const [elapsed, setElapsed] = useState(0);
+
+  const timeScale = opts?.timeScale || 1;
 
   useEffect(() => {
     if (startTime) {
       const interval = setInterval(() => {
         const now = new Date();
-        const msElapsed = now.getTime() - startTime.getTime();
+        const msElapsed = (now.getTime() - startTime.getTime()) * timeScale;
         if (msElapsed > duration * 1000) {
           setElapsed(duration);
           clearInterval(interval);
         } else {
           setElapsed(msElapsed / 1000);
         }
-      }, 500);
+      }, 50);
 
       return () => clearInterval(interval);
     }
